85a94a5b2faf9e2b551c561edda96d000cd87dad
PythonShinobi <pythonshinobi@gmail.com>
2024-06-18 17:36:02 +0300

    Add route configuration for ProductsPage and ProductDetailsPage.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import "./App.css"
 import AdminForm from './admin/AdminForm';
+import ProductsPage from './product/ProductsPage';
+import ProductDetailsPage from './product/ProductsDetailsPage';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Routes>
         {/* Define routes for different pages */}
         <Route path="/admin" element={<AdminForm />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailsPage />} />
       </Routes>
     </BrowserRouter>
   );
