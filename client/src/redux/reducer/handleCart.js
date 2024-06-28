e3652f53895b5befe84f48f997aa845e6b719a8e
PythonShinobi <pythonshinobi@gmail.com>
2024-06-28 11:29:42 +0300

    Add a case to handle the clearing of the cart items in handleCart reducer.

--- client/src/redux/reducer/handleCart.js
+++ client/src/redux/reducer/handleCart.js
@@ -1,44 +1,42 @@
 // client/src/redux/reducer/handleCart.js
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-// Reducers are pure functions that take the current state and an action as arguments and return a 
-// new state. They specify how the state changes in response to actions.
-
-const cart = [];
-
-// The 'handleCart' reducer manages the state related to the shopping cart 
-// updating it based on the dispatched actions.
-const handleCart = (state=cart, action) => {
+const handleCart = (state = cart, action) => {
   const product = action.payload;
+  let newState;
+
   switch (action.type) {
     case "ADDITEM":
-      // Check if the product is already in the cart
       const exist = state.find((x) => x.id === product.id);
-
-      // If the product is already in the cart
       if (exist) {
-          // Increase the quantity of the existing product
-          return state.map((x) => x.id === product.id ? { ...x, qty: x.qty + 1 } : x);
+        newState = state.map((x) =>
+          x.id === product.id ? { ...x, qty: x.qty + 1 } : x
+        );
       } else {
-          // If the product is not in the cart, add it with a quantity of 1
-          return [...state, { ...product, qty: 1 }];
-      }            
+        newState = [...state, { ...product, qty: 1 }];
+      }
+      localStorage.setItem("cart", JSON.stringify(newState));
+      return newState;
 
     case "DELITEM":
-      // Find the item in the cart that matches the product ID
       const delItem = state.find((x) => x.id === product.id);
-      
-      // Check if the quantity of the item to be deleted is 1
       if (delItem.qty === 1) {
-          // If the quantity is 1, remove the item from the cart
-          return state.filter((x) => x.id !== delItem.id);
+        newState = state.filter((x) => x.id !== delItem.id);
       } else {
-          // If the quantity is greater than 1, decrement the quantity by 1
-          return state.map((x) => x.id === product.id ? {...x, qty: x.qty - 1} : x)
-      } 
+        newState = state.map((x) =>
+          x.id === product.id ? { ...x, qty: x.qty - 1 } : x
+        );
+      }
+      localStorage.setItem("cart", JSON.stringify(newState));
+      return newState;
+
+    case "CLEARCART":
+      localStorage.removeItem("cart");
+      return [];
 
     default:
-      return state
-  };
+      return state;
+  }
 };
 
 export default handleCart;
