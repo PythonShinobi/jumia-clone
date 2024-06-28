3101cc289a49001deb41b521800e117d17aaf5eb
PythonShinobi <pythonshinobi@gmail.com>
2024-06-28 08:03:24 +0300

    Add route configuration for login, register, checkout and pagenotfound.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import "./App.css"
+import "./App.css";
 import store from './redux/store';
 import AdminForm from './admin/AdminForm';
 import ProductsPage from './product/ProductsPage';
@@ -12,6 +12,12 @@ import Home from './home/Home';
 import About from './about/About';
 import ContactPage from './contact/Contact';
 import Cart from './cart/Cart';
+import Login from './login/login';
+import Register from './register/register';
+import Checkout from './checkout/Checkout';
+import PageNotFound from './404-page/PageNotFound';
+import PrivateRoute from './components/PrivateRoute';
+import AdminPage from './components/AdminPage';
 
 const App = () => {
   return (
@@ -21,11 +27,36 @@ const App = () => {
           {/* Define routes for different pages */}
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/admin" element={<AdminForm />} />
+          <Route path="/contact" element={<ContactPage />} />          
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/product/*" element={<PageNotFound />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/products/:id" element={<ProductDetailsPage />} />
+          <Route 
+            path="/checkout" 
+            element={
+              <PrivateRoute>
+                  <Checkout />
+              </PrivateRoute>
+            } />
+          <Route 
+            path="/admin" 
+            element={
+              <AdminPage>
+                <AdminForm />
+              </AdminPage>
+            } 
+          />
+          <Route
+            path="/cart"
+            element={
+              <PrivateRoute>
+                <Cart />
+              </PrivateRoute>
+            }
+        />
         </Routes>
       </Provider>
     </BrowserRouter>
