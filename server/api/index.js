511b037e10588edacfdae8f4bfa28f823d126ed3
PythonShinobi <pythonshinobi@gmail.com>
2024-07-01 13:07:12 +0300

    change allowedOrigins from LOCALHOST to HOST.

--- server/api/index.js
+++ server/api/index.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.SERVER_PORT || 5001
 
 // Define the origins from which the frontend will be making requests.
-const allowedOrigins = [ process.env.LOCALHOST ]
+const allowedOrigins = [ process.env.HOST ]
 
 // Configure CORS to allow requests from specified origins.
 app.use(cors({
